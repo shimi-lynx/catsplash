644d390e48b722cf2d335c98bdd348061156494e
shimi-lynx <n33etc@gmail.com>
2020-06-18 20:25:19 +0900

    feat: ユーザ登録機能に登録確認メールの追加
    - ユーザ登録時にアカウント有効化メールを送付

--- back/app/models/account.rb
+++ back/app/models/account.rb
@@ -4,7 +4,7 @@ class Account < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
   has_many :photos, dependent: :destroy
