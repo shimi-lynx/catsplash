3401b2f18e978370ddef0cf9e26c19cb62e72d4e
shimi-lynx <n33etc@gmail.com>
2020-06-23 10:32:26 +0900

    update: 認証機能に成功・失敗メッセージを追加
    - ログイン後に「ログインしました」等、認証周りの処理にtoastメッセージを追加
    - ユーザ登録API成功時にアカウント有効化メールの確認を促すページへ遷移する処理追加

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import PhotoList from "../views/PhotoList.vue";
 import TestPhotoList from "../views/TestPhotoList.vue";
 import Auth from "../views/Auth.vue";
+import confirm from "../views/auth/confirm.vue";
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,11 @@ const routes = [
     path: "/auth",
     name: "Auth",
     component: Auth
+  },
+  {
+    path: "/auth/confirm",
+    name: "confirm",
+    component: confirm
   },
   {
     path: "/test",
---
+++ front/src/views/auth/confirm.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container is-fluid">
+    <div class="columns is-centered is-marginless">
+      <div class="column is-three-quarters">
+        <div class="content is-large">ユーザ登録確認の為にメールを送信しました。メール本文の「アカウントを有効化する」をクリックしてユーザ登録を完了させて下さい。</div>
+      </div>
+    </div>
+  </div>
+</template>
